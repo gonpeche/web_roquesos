a4e32867f5ca264c1394c9d89f9f8ef3146eb790
gonpeche <espeche.g@gmail.com>
2018-09-05 18:12:11 -0300

    update form php

--- mail.php
+++ mail.php
@@ -2,9 +2,9 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "espeche.g@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Consulta enviada con éxito! Nos comunicaremos con vos a la breveded. Gracias!" . " -" . "<a href='http://roquito.gonpeche.com' style='text-decoration:none;color:#ff0099;'> Volver</a>";
 ?>
